19d218e884a396d3314638d27ed74f09eb2a6d51
彭萨萨 <pengsasa@qingzu.com.cn>
2019-12-11 17:07:55 +0800

    22

--- src/main/java/com/example/test/controller/Hello.java
+++ src/main/java/com/example/test/controller/Hello.java
@@ -1,10 +1,12 @@
 package com.example.test.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class Hello {
@@ -14,6 +16,7 @@ public class Hello {
 
     @GetMapping("/hello")
     public String test(){
+        log.info("#################进入了");
         return name;
     }
 }
