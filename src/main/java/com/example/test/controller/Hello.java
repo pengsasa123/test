89d1874380bc3247fc380d2fec3eb162c513c5ac
彭萨萨 <pengsasa@qingzu.com.cn>
2019-12-11 16:49:21 +0800

    nacos

--- src/main/java/com/example/test/controller/Hello.java
+++ src/main/java/com/example/test/controller/Hello.java
@@ -1,5 +1,6 @@
 package com.example.test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class Hello {
 
+    @Value("${user.name}")
+    private String name;
 
     @GetMapping("/hello")
     public String test(){
-        return "hello";
+        return name;
     }
 }
