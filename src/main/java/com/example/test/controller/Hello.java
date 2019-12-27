9d2849ba131bde177e1adc922a8ecd650383f2b6
彭萨萨 <pengsasa@qingzu.com.cn>
2019-12-27 11:23:02 +0800

    x

--- src/main/java/com/example/test/controller/Hello.java
+++ src/main/java/com/example/test/controller/Hello.java
@@ -1,15 +1,20 @@
 package com.example.test.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
 @RequestMapping("/test")
 public class Hello {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${user.name}")
     private String name;
@@ -18,5 +23,12 @@ public class Hello {
     public String test(){
         log.info("#################进入了");
         return name;
+    }
+
+
+    @GetMapping("/test")
+    public String test2(){
+        String forObject = restTemplate.getForObject("http://qingzu/test/hello", String.class);
+        return forObject;
     }
 }
