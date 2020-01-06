0ea1d0ec87244bf1320c69905e2cfe69264cafdf
彭萨萨 <pengsasa@qingzu.com.cn>
2020-01-06 16:42:26 +0800

    dafds

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -1,15 +1,19 @@
 package com.example.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TestApplication {
 
     public static void main(String[] args) {
+        log.info("------------start------------");
         SpringApplication.run(TestApplication.class, args);
+        log.info("------------end------------");
     }
 
 }
